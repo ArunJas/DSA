c0ed559aef086c169cf56b492671664b6f39f6a1
Arun Jasrotia <jasrotiaarun099@gmail.com>
2026-05-08 04:27:19 -0700

    change

--- lec5/bitwiseopp.cpp
+++ lec5/bitwiseopp.cpp
@@ -1,8 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-cout <<"goo";
+ int main (){
+   int a = 4;
+   int b = 6;
 
-
-}
+      cout <<" a&b "<< (a&b) << endl ;
+      cout <<" a|b "<< (a|b) << endl ;
+      cout <<" ~a "<< ~a << endl ;
+      cout <<" a^b "<< (a^b) << endl ;
+   
+     }
---
+++ lec5/forloop4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+     for(int k = 0;k<5;k++){
+        cout << "hello Annish " << endl;
+           cout << " hey Annish" << endl ;
+             continue;
+                 cout << "reply toh karde " << endl;
+                 
+                 }
+
+
+
+}
